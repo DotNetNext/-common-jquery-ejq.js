f1ccb2b5ae660624bd9510932fbb44c6b3d0bbfc
sunkaixuan <610262374@qq.com>
2016-08-03 18:04:36 +0800

    ok

---
+++ jquery-ng/JQuery-ng/Web/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Web.Startup))]
+namespace Web
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            
+        }
+    }
+}
